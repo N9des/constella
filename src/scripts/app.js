8fef1762b793ab0772a32ba973a17f8b3c6212b5
Dimigia <dimigia.design@gmail.com>
2021-05-23 15:44:51 +0200

    Ajout filtre Boreal et Australe

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -12,9 +12,12 @@ let constWindow = document.querySelector(".star");
 let closeButton = document.querySelector("#trigger");
 let mainTitle = document.querySelector(".title--medium");
 let zodiaqueFilter = document.querySelector('#zodiaque-filter');
+let borealFilter = document.querySelector('#boreal-filter');
+let australFilter = document.querySelector('#austral-filter');
 
 let data; // <-- On va stocker les infos du json ici
 /* STEP 1: Créer une fonction qu'on va appeler APRES avoir charger le JSON */
+
 function initHoverInteractions () {
     var paths = document.querySelectorAll("[data-name]");
     paths.forEach(function (path){
@@ -41,13 +44,8 @@ var activeArea = function (id) {
         return elm.num.indexOf(id)>=0
     }); */
 
-    let constList = data.constellations 
     let chosenConst = data.constellations.find(constellation => constellation.num == id);
     let rawData = Object.values(chosenConst);
-
-    
-    
-
 
 
     let constName =         rawData[0];
@@ -69,23 +67,90 @@ var activeArea = function (id) {
     
 }
 
-zodiaqueFilter.addEventListener('click', function(){
-    let constList = data.constellations 
+function initFilterActions(){
+    let constList = data.constellations;
 
-        for (let i = 0; i < constList.length; i++)
-        {
+    function resetFilters(){
+        for (let i = 0; i < constList.length; i++){
             let selectedConst = constList[i];
             let selectedConstElement = document.querySelector("[data-name='"+selectedConst.num+"']")
-            if(!selectedConstElement){
-                continue;
-            }
 
-            if (selectedConst.zodiaque === false) 
-            {
-                selectedConstElement.classList.toggle("not-selected");
+            console.log(selectedConstElement);
+            if (selectedConstElement !== null && selectedConstElement.classList.contains("not-selected")){
+            selectedConstElement.classList.remove("not-selected");
             }
         }
-})
+    }
+    
+    function toggleZodiacSigns(){
+        resetFilters();
+        borealFilter.checked = false;
+        australFilter.checked = false;
+        if (zodiaqueFilter.checked == true){
+            for (let i = 0; i < constList.length; i++)
+            {
+                let selectedConst = constList[i];
+                let selectedConstElement = document.querySelector("[data-name='"+selectedConst.num+"']")
+                if(!selectedConstElement){
+                    continue;
+                }
+                
+                if (selectedConst.zodiaque === false) 
+                {
+                    selectedConstElement.classList.add("not-selected");
+                }
+            }
+        }else{
+            resetFilters();  
+        }
+
+    }
+    
+    zodiaqueFilter.addEventListener('click', toggleZodiacSigns);
+
+    function toggleBorealSigns (){
+        resetFilters();
+        zodiaqueFilter.checked = false;
+        australFilter.checked = false;
+        if (borealFilter.checked == true){
+            for (let i = 0; i < constList.length; i++){
+                let selectedConst = constList[i];
+                let selectedConstElement = document.querySelector("[data-name='"+selectedConst.num+"']")
+                if(!selectedConstElement){
+                    continue;
+                }
+                if (selectedConst.boreal === false){
+                    selectedConstElement.classList.add("not-selected");
+                }
+            }
+        }else{
+            resetFilters();
+        }
+    }
+    australFilter.addEventListener('click', toggleAustralSigns);
+
+    function toggleAustralSigns (){
+        resetFilters();
+        zodiaqueFilter.checked = false;
+        borealFilter.checked = false;
+        if (australFilter.checked == true){
+            for (let i = 0; i < constList.length; i++){
+                let selectedConst = constList[i];
+                let selectedConstElement = document.querySelector("[data-name='"+selectedConst.num+"']")
+                if(!selectedConstElement){
+                    continue;
+                }
+                if (selectedConst.australe === false){
+                    selectedConstElement.classList.add("not-selected");
+                }
+            }
+        }else{
+            resetFilters();
+        }
+    }
+    borealFilter.addEventListener('click', toggleBorealSigns);
+
+}
 
 
 
@@ -99,4 +164,6 @@ fetch(myRequest)
         data = jsonData; // <-- On sauve les infos du json dans notre variable Data
         // Maintenant que le JSON est chargé, on peut démarrer à écouter les interactions
         initHoverInteractions();
+        initFilterActions();
     });
+
