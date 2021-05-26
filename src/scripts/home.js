630bccb545373717baf467fee17777c83ba77370
Dimigia <dimigia.design@gmail.com>
2021-05-26 17:31:02 +0200

    Animation home GSAP

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -20,6 +20,9 @@ let sect02Img = document.querySelector(".const-img");
 let sect03Art = document.getElementById("sect03-article");
 let sect03Img = document.querySelector(".box-info-img");
 
+let scrollV = 0;
+let shown1 = false;
+let shown2 = false;
 window.addEventListener('scroll', function(){
 
   let scrollValue = (window.innerHeight + window.scrollY) / (document.body.offsetHeight);
@@ -31,13 +34,48 @@ window.addEventListener('scroll', function(){
     sect02Art.style.transform = 'none';
     sect02Img.style.opacity = '1';
     sect02Img.style.transform = 'none';
+  if(scrollValue > scrollV){
+    scrollV = scrollValue;
+    console.log(scrollValue);
   }
 
+  console.log(scrollV);
+  if(scrollValue > 1.6){
+
+    function showContent(){
+
+      if (shown1 == false){
+        shown1 = true;
+        let tl = gsap.timeline();
+        tl.to(".sect--02-article", {duration: 1, y: 0})
+        tl.to(".sect--02-article", {duration: 2, opacity: 1},"-=1")
+        tl.to(".sect--02-image", {duration: 1.75, opacity: 1}, "-=2");
+        tl.to(".sect--02-image", {duration: 1.5, y: 0}, "-=2.5")
+
+      }
+    }
+    showContent();
+
+  }
+  
   if(scrollValue > 2.5){
     sect03Art.style.opacity = '1';
     sect03Art.style.transform = 'none';
     sect03Img.style.opacity = '1';
     sect03Img.style.transform = 'none';
+    
+    function showContent2(){
+      if (shown2 == false){
+        shown2 = true;
+        let tl = gsap.timeline();
+        tl.to(".sect--03-article", {duration: 1, y: 0})
+        tl.to(".sect--03-article", {duration: 2, opacity: 1},"-=1")
+        tl.to(".sect--03-image", {duration: 1.75, opacity: 1}, "-=2");
+        tl.to(".sect--03-image", {duration: 1.5, y: 0}, "-=2.5")
+
+      }
+    }
+    showContent2();
   }
 
 
