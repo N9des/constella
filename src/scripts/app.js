36c6f263a8e578d4d3c4eb433c28cce0ca1dbd70
Dimigia <dimigia.design@gmail.com>
2021-05-26 12:05:47 +0200

    Javascript counter

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -89,6 +89,28 @@ function animateValue(obj, start, end, duration) {
       const progress = Math.min((timestamp - startTimestamp) / duration, 1);
       obj.innerHTML = Math.floor(progress * (end - start) + start)+" deg2";
       if (progress < 1) {
+let size = taille.innerHTML.replace(" deg2", "");
+let classement = ordre.innerHTML.replace("ème", "");
+let highlights = [size, classement];
+console.log(highlights);
+
+for (let i = 0; i < highlights.length; i++){
+
+    function animateValue(selectedHighlight, start, end, duration) {
+        let startTimestamp = null;
+        const step = (timestamp) => {
+          if (!startTimestamp) startTimestamp = timestamp;
+          const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+          
+          if (i == 0){
+            taille.innerHTML = Math.floor(progress * (end - start) + start)+" deg2";
+          }else{
+            ordre.innerHTML = Math.floor(progress * (end - start) + start)+"ème";
+          }
+          if (progress < 1) {
+            window.requestAnimationFrame(step);
+          }
+        };
         window.requestAnimationFrame(step);
       }
     };
@@ -97,6 +119,11 @@ function animateValue(obj, start, end, duration) {
   
   const obj = document.getElementById('taille');
   animateValue(obj, 100, classement, 450);
+      
+      let selectedHighlight = highlights[i];
+      animateValue(selectedHighlight, 100, selectedHighlight, 450);
+    }
+
 }
 
 function initFilterActions(){
